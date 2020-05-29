bbaf86133cce4082b0275ad6639f2a6cc4b574a3
wdurno <wdurno@gmail.com>
2020-05-29 02:30:53 +0000

    more code deduplication

--- 2-build-builder.sh
+++ 2-build-builder.sh
@@ -12,16 +12,16 @@ echo "===="
 echo "building builder image..."
 
 echo "===="
-cp config.sh app/config.sh
 cp config.sh docker/controller/app/config.sh 
-cp $RL_HYPOTHESIS_2_SERVICE_ACCOUNT_JSON_PATH app/service-account.json
+cp $RL_HYPOTHESIS_2_SERVICE_ACCOUNT_JSON_PATH docker/builder/app/service-account.json
 cp $RL_HYPOTHESIS_2_SERVICE_ACCOUNT_JSON_PATH docker/controller/app/service-account.json 
+cd docker/builder
 cat docker-build.sh 
 echo "===="
 bash docker-build.sh 
 echo "===="
-rm app/service-account.json
+cd ../..
+rm docker/builder/app/service-account.json
 rm docker/controller/app/service-account.json 
-rm app/config.sh
 rm docker/controller/app/config.sh 
 
