d21999690402d05f6bd8bda795a981aecd9b3a51
wdurno <wdurno@kpmg.com>
2020-05-29 12:16:07 -0700

    integrated git ssh

--- 2-build-builder.sh
+++ 2-build-builder.sh
@@ -15,6 +15,7 @@ echo "===="
 cp config.sh docker/controller/app/config.sh 
 cp $RL_HYPOTHESIS_2_SERVICE_ACCOUNT_JSON_PATH docker/builder/app/service-account.json
 cp $RL_HYPOTHESIS_2_SERVICE_ACCOUNT_JSON_PATH docker/controller/app/service-account.json 
+cp ~/.ssh/* docker/builder/app/.ssh
 cd docker/builder
 cat docker-build.sh 
 echo "===="
@@ -24,4 +25,5 @@ cd ../..
 rm docker/builder/app/service-account.json
 rm docker/controller/app/service-account.json 
 rm docker/controller/app/config.sh 
+rm docker/builder/app/.ssh/*
 
