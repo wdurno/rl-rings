d97b279e60205ffe0ff1dd605c57da7aed3b1652
wdurno <wdurno@gmail.com>
2020-05-29 03:21:53 +0000

    saving progress...

--- docker/controller/app/entrypoint.sh
+++ docker/controller/app/entrypoint.sh
@@ -1,2 +1,14 @@
-cd /app/python
-python3 -u actor_critic.py 
+_JOB=$1
+# Override JOB if arg given
+if [ -z "$_JOB" ]; then echo "JOB=$JOB"; else export JOB=$_JOB; fi
+
+if [ $JOB == "0-in" ]; then
+        echo "Interactive mode..."
+fi
+
+if [ $JOB == "8-ac" ]; then
+        echo "Run actor critic..."
+	cd /app/python
+	python3 -u actor_critic.py 
+fi
+
