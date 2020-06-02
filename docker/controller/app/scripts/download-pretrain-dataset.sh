08fdffd14f0dc613ded285f354641b5f07edbad2
wdurno <wdurno@gmail.com>
2020-06-02 04:29:08 +0000

    removed docker login

--- docker/controller/app/scripts/download-pretrain-dataset.sh
+++ docker/controller/app/scripts/download-pretrain-dataset.sh
@@ -1,6 +1,5 @@
 source /app/config.sh 
 gcloud auth activate-service-account $RL_HYPOTHESIS_2_SERVICE_ACCOUNT_NAME --key-file /app/service-account.json || exit 1 
-cat /app/service-account.json | docker login -u _json_key --password-stdin https://${RL_HYPOTHESIS_2_DOCKER_REGISTRY_HEAD} || exit 1 
 mkdir -p /dat/pre-train
 gsutil cp ${RL_HYPOTHESIS_2_PRETRAIN_DATA} /dat/pre-train/pre-train.tar.gz || exit 1
 cd /dat/pre-train
