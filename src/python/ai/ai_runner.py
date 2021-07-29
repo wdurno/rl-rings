676f54302caf738bfc5c78b1ace2550ecb302721
wdurno <wdurno@gmail.com>
2021-07-29 03:43:43 +0000

    drafted batch norms

--- src/python/ai/ai_runner.py
+++ src/python/ai/ai_runner.py
@@ -55,48 +55,67 @@ class CNN(nn.Module):
         self.default_device = default_device 
         # convs 
         self.conv1 = nn.Conv2d(in_channels=3, out_channels=32, kernel_size=3, stride=1) 
-        self.conv2 = nn.Conv2d(32, 64, kernel_size=3, stride=1)
-        self.conv3 = nn.Conv2d(64, 128, kernel_size=3, stride=1)
+        self.conv1_bn = nn.BatchNorm2d(32) 
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=3, stride=1) 
+        self.conv2_bn = nn.BatchNorm2d(64) 
+        self.conv3 = nn.Conv2d(64, 128, kernel_size=3, stride=1) 
+        self.conv3_bn = nn.BatchNorm2d(128) 
         self.conv4 = nn.Conv2d(128, 256, kernel_size=3, stride=2) 
-        # max pool 
+        self.conv4_bn = nn.BatchNorm2d(256) 
         self.conv5 = nn.Conv2d(256, 512, kernel_size=3, stride=1) 
+        self.conv5_bn = nn.BatchNorm2d(512) 
         self.conv6 = nn.Conv2d(512, 1024, kernel_size=3, stride=1) 
+        self.conv6_bn = nn.BatchNorm2d(1024) 
         self.conv7 = nn.Conv2d(1024, 2048, kernel_size=3, stride=1) 
-        self.conv8 = nn.Conv2d(2048, 4096, kernel_size=3, stride=2)
+        self.conv7_bn = nn.BatchNorm2d(2048) 
+        self.conv8 = nn.Conv2d(2048, 4096, kernel_size=3, stride=2) 
+        self.conv8_bn = nn.BatchNorm2d(4096) 
         #dropout layer
         self.conv_drop = nn.Dropout2d()
         #fully connected layer
         self.fc1 = nn.Linear(4096*1*1, 100) 
+        self.fc1_bn = nn.BatchNorm1d(100) 
         self.fc2 = nn.Linear(100, 20) 
+        self.fc2_bn = nn.BatchNorm1d(20) 
         self.fc3 = nn.Linear(20+1, N_OPS) # 20 for pov, +1 for vec 
 
     def forward(self, obs): 
         obs = self.__format_observation(obs) 
         x = obs['pov'] 
         x = self.conv1(x) 
+        x = self.conv1_bn(x) 
         x = F.relu(x) 
         x = self.conv2(x) 
+        x = self.conv2_bn(x) 
         x = F.relu(x) 
         x = self.conv3(x) 
+        x = self.conv3_bn(x) 
         x = F.relu(x) 
         x = self.conv4(x) 
+        x = self.conv4_bn(x) 
         x = F.max_pool2d(x, 2)
         x = F.relu(x) 
         x = self.conv5(x) 
+        x = self.conv5_bn(x) 
         x = F.relu(x) 
         x = self.conv6(x) 
+        x = self.conv6_bn(x) 
         x = F.relu(x) 
         x = self.conv7(x) 
+        x = self.conv7_bn(x) 
         x = F.relu(x) 
-        x = self.conv8(x) # returns shape [4096, 3, 3]
+        x = self.conv8(x) # returns shape [4096, 3, 3] 
+        x = self.conv8_bn(x) 
         x = self.conv_drop(x)
         x = F.max_pool2d(x, 2) # returns shape [4096, 1, 1] 
         x = F.relu(x)
         x = x.view(-1, 4096*1*1) 
-        x = self.fc1(x)
+        x = self.fc1(x) 
+        x = self.fc1_bn(x) 
         x = F.relu(x)
         x = F.dropout(x)
         x = self.fc2(x)
+        x = self.fc2_bn(x) 
         x = F.relu(x) 
         v = obs['vec'] 
         x = torch.cat([x,v], dim=1) 
